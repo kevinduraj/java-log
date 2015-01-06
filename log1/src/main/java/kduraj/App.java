415b034185b545c7d99086568843f39ade4f8996
Kevin Duraj <kevinduraj@gmail.com>
2015-01-06 15:28:45 -0800

    ch.qos.logback

--- log1/src/main/java/kduraj/App.java
+++ log1/src/main/java/kduraj/App.java
@@ -7,8 +7,10 @@ public class App {
 
     public static void main(String[] args) {
 
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE");
+
         Logger logger = LoggerFactory.getLogger(App.class);
-        
+
         logger.trace("logger.trace");
         logger.debug("logger.debug");
         logger.info("logger.info");
