30b10969202139a422e3b2d87378bc8e9ee87403
Kevin Duraj <kevinduraj@gmail.com>
2015-01-06 15:20:37 -0800

    logger:trace,debug,info,warn,error

--- log1/build.sh
+++ log1/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #------------------------------------------------------#
 clear; time mvn clean compile assembly:single
-#java -jar target/log1-1.0.0-jar-with-dependencies.jar
+java -jar target/log1-1.0.0-jar-with-dependencies.jar
 #------------------------------------------------------#
--- log1/src/main/java/kduraj/App.java
+++ log1/src/main/java/kduraj/App.java
@@ -1,9 +1,18 @@
 package kduraj;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(App.class);
+        
+        logger.trace("logger.trace");
+        logger.debug("logger.debug");
+        logger.info("logger.info");
+        logger.warn("logger.warn");
+        logger.error("logger.error");
     }
 }
