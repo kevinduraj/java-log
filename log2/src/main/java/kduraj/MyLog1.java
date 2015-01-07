13e1d065a399222b493aab2f97a169e9ca929428
Kevin Duraj <kevinduraj@gmail.com>
2015-01-06 17:19:41 -0800

    log2

---
+++ log2/src/main/java/kduraj/MyLog1.java
@@ -0,0 +1,47 @@
+package kduraj;
+
+import org.apache.log4j.Logger;
+
+/**----------------------------------------------------------------------------------
+ *  java -cp logger.jar kduraj.MyLog1
+ -----------------------------------------------------------------------------------*/
+public class MyLog1 {
+
+    final static Logger logger = Logger.getLogger(MyLog1.class);
+    /*------------------------------------------------------------------------------*/
+
+    public static void main(String[] args) {
+
+        MyLog1 obj = new MyLog1();
+        obj.test1();
+        //obj.test2("logging...");
+
+    }
+
+    /*------------------------------------------------------------------------------*/
+    public static void test1() {
+        System.out.println("true = " + true);
+    }
+    /*------------------------------------------------------------------------------*/
+
+    /*
+    private void test2(String parameter) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("This is debug : " + parameter);
+        }
+
+        if (logger.isInfoEnabled()) {
+            logger.info("This is info : " + parameter);
+        }
+
+        logger.warn("This is warn : " + parameter);
+        logger.error("This is error : " + parameter);
+        logger.fatal("This is fatal : " + parameter);
+
+    }
+    */
+
+    /*------------------------------------------------------------------------------*/
+
+}
