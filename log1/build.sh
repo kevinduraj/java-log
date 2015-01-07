1c6ae513a887a7c7a3490f78bae42aa6ad106d04
Kevin Duraj <kevinduraj@gmail.com>
2015-01-06 18:38:32 -0800

    rename

--- log1/build.sh
+++ log1/build.sh
@@ -2,7 +2,7 @@
 #-----------------------------------------------------#
 mvn clean install
 cd target
-CLASSPATH=logger.jar:~/.m2/repository/log4j/log4j/1.2.17/*
+CLASSPATH=logger1.jar:~/.m2/repository/log4j/log4j/1.2.17/*
 java -cp $CLASSPATH kduraj.MyLogger
 #-----------------------------------------------------#
 
